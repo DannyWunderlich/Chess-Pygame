4d18d90452d03efd6855787696f4f1cf85e3af2c
DannyWunderlich <143848492+DannyWunderlich@users.noreply.github.com>
2026-01-29 17:17:51 -0500

    made new board without border

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 import pygame
 
+#DIMENSIONS
+
 SCREEN_WIDTH = 512
 SCREEN_HEIGHT = 512
 
+SQUARE_WIDTH = 64
+SQUARE_HEIGHT = 64
+
+#COLORS
+
+PINK = (211, 187, 227)
+WHITE = (255, 255, 255)
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
 def init_pieces():
     # TODO
-    # Initialize all the pieces on black and black side
+    # Initialize all the pieces on white and black side
+    # ==== Need to fix sizing and placement issue with the pieces ====
     black_pawn = pygame.image.load("assets/black_pawn.png")
     shrunk_black_pawn = pygame.transform.smoothscale(black_pawn, (51, 51))
+
     return shrunk_black_pawn
-    
+
 def init_board():
-    board = pygame.image.load("assets/board.png")
-    shrunk_board = pygame.transform.smoothscale(board, (512, 512))
-    return shrunk_board
+    squares_created = 0
+
+    for x_pos in range(0, SCREEN_WIDTH, SQUARE_WIDTH):
+        for y_pos in range(SCREEN_HEIGHT, -1, -1 * SQUARE_HEIGHT):
+            if(squares_created % 2):
+                game_board = pygame.Rect(x_pos, y_pos, 64, 64) #create rectangle object and display it to the screen
+                pygame.draw.rect(screen, PINK, game_board)
+            else:
+                game_board = pygame.Rect(x_pos, y_pos, 64, 64) #create rectangle object and display it to the screen
+                pygame.draw.rect(screen, WHITE, game_board)
+
+                squares_created += 1
 
 def main():
     # Example file showing a basic pygame "game loop"
 
     # pygame setup
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
     running = True
 
     while running:
@@ -30,14 +52,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                
 
         # RENDER YOUR GAME HERE
+        init_board()
 
-        chess_board = init_board()
-        black_pawn = init_pieces()
-
-        screen.blit(chess_board, dest=(0, 0))
-        screen.blit(black_pawn, dest=(51, 51))
+        # print(pygame.mouse.get_pos())
 
         # flip() the display to put your work on screen
         pygame.display.flip()
